0878a568dcb9e5c3dab642efe012dfe180702d74
Hedi Boissard <hboissard9@gmail.com>
2025-02-12 20:18:40 +0100

    Ajoute de la route utlisateur et d'une fonction getAllUtilisateur + d'une fonction addUtilisateur

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Book = require('./models/book');
+const Utilisateur = require('./models/utilisateur');
 require ('dotenv').config();
 
 const app = express();
@@ -7,13 +7,31 @@ app.use(express.json());
 
 //EndPoints
 
-app.get('/books', async (req, res) => {
+app.get('/utilisateurs', async (req, res) => {
     try {
-        const books = await Book.getAllBooks();
-        res.status(200).json(books);
-        res.json(books);
+        const utilisateur = await Utilisateur.getAllUtilisateur();
+        res.status(200).json(utilisateur);
+        res.json(utilisateur);
     }   catch (error) {        
         res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/utilisateurs', async (req, res) => {
+    try {
+        const { nom, prenom, email, mot_de_passe } = req.body;
+        const sucess = `Utilisateur ajouté avec succès ${nom} !`;	
+        if (!nom || !prenom || !email || !mot_de_passe) {
+            return res.status(400).json({ message: 'Tous les champs sont requis' });
+        }
+
+        const result = await Utilisateur.addUtilisateur(nom, prenom, email, mot_de_passe);
+        console.log(sucess);
+
+        res.status(201).json({ message: 'Utilisateur ajouté avec succès', result });
+    } catch (error) {
+        console.error('Erreur lors de l\'ajout de l\'utilisateur :', error);
+        res.status(500).json({ message: 'Erreur interne du serveur' });
     }
 });
 
